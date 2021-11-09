8d1aac3e2285d9924f7d0049160356bd7ab0e5a3
Preston Dunton <preston.dunton@gmail.com>
2021-11-09 10:05:21 -0700

    Sorted pageRank and cleaned up file

--- src/main/scala/PageRank.scala
+++ src/main/scala/PageRank.scala
@@ -1,6 +1,6 @@
 import org.apache.spark.sql.SparkSession
 
-object WordCount {
+object PageRank {
 
   def main(args: Array[String]): Unit = {
 
@@ -17,9 +17,13 @@ object WordCount {
 
 
     val lines = sc.textFile(args(0))
+
     //val numLines = scala.io.Source.fromFile(args(0)).getLines.size
     val links = lines.map(s => (s.split(": ")(0), s.split(": ")(1)))
+
+    // Used for creating our titleIndexRank output
     val titles = sc.textFile(args(1)).zipWithIndex().mapValues(x => x + 1).map(_.swap)
+
     val count = lines.count()
     var ranks = links.mapValues(v => 1.0 / count)
     for (i <- 1 to 25) {
@@ -30,10 +34,17 @@ object WordCount {
       }
       ranks = tempRank.reduceByKey(_ + _)
     }
-    ranks.saveAsTextFile(args(2))
-    titles.saveAsTextFile(args(3))
+    
     val titleIndexRank = titles.map(x=>(x._1.toString,x._2))
     val titleRank = (titleIndexRank.join(ranks)).values
-    titleRank.sortBy(_._2,false).saveAsTextFile(args(4))
+
+     // sort by page rank decreasing
+    ranks = ranks.sortBy(_._2, false)
+    titleRank.sortBy(_._2,false)
+
+    // save outputs
+    ranks.saveAsTextFile(args(2))
+    titles.saveAsTextFile(args(3))
+    titleRank.saveAsTextFile(args(4))
   }
 }
